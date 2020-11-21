00a58ffc044ddf2431011806483a1549dbced4f7
yishangru <yishangru@126.com>
2020-11-21 01:31:31 -0500

    project: initial LS

---
+++ solution/localSearchSol.py
@@ -0,0 +1,17 @@
+"""
+This is a solution retrieved using local search Sol.
+"""
+
+from solution.solution import Solution
+
+class localSearch1Sol(Solution):
+    def __init__(self, graph, randomSeed, startTime, parameterDict):
+        super().__init__(graph, randomSeed, startTime)
+        self.parameterDict = parameterDict
+
+    # override parent method
+    def run(self):
+        # Cai, Shaowei, Jinkun Lin, and Kaile Su.
+        # "Two weighting local search for minimum vertex cover."
+        # In Proceedings of the Twenty-Ninth AAAI Conference on Artificial Intelligence, pp. 1107-1113. 2015.
+        pass
