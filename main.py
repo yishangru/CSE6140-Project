cdf583f5626f87744605157d85fcd32d5fd88f17
yishangru <yishangru@126.com>
2020-11-26 01:31:00 -0500

    project: add repeat run

--- main.py
+++ main.py
@@ -11,7 +11,7 @@ from utils.data import checkData, readData, checkSol, writeSol, writeTrace
 DEBUG = True
 defaultProcessNum = 1
 graphDataDirectory = "./data/Data"
-
+debugResultDirectory = "./result"
 
 def main():
     parser = argparse.ArgumentParser()
@@ -22,7 +22,9 @@ def main():
     parser.add_argument('-time', type=int, default=200,
                         help="time limit for algorithm run, default 200")
     parser.add_argument('-seed', type=int, default=-1,
-                        help="Random seed, default -1")
+                        help="random seed, default -1")
+    parser.add_argument('-rc', type=int, default=1,
+                        help="algorithm running repeat count")
     parser.add_argument('-cd', '--checkData', action="store_true", default=False,
                         help="check graph data validity")
     parser.add_argument('-cs', '--checkSol', action="store_true", default=False,
@@ -50,8 +52,14 @@ def main():
     else:
         target_graph_list.append(args.inst)
 
+    # make directory for debug running results
+    if DEBUG:
+        if not (os.path.exists(debugResultDirectory) and os.path.isdir(debugResultDirectory)):
+            os.mkdir(debugResultDirectory)
+
     for i in range(len(target_graph_list)):
         graph_path = target_graph_list[i]
+
         print("Current Running [ " + graph_path + " ]")
 
         # check graph valid
@@ -61,57 +69,63 @@ def main():
             print()
             continue
 
-        # start running
-        start_time = time.time()
-
-        # read data
-        graph = readData(graph_path)
-        param_json = json.loads(args.params)
-
-        """
-            Using multiprocessing for possible multiple process concurrent running.
-            Multiprocessing will serialize and deserialize the argument (deep copy). 
-            Thus, each process will have it independent memory space (not affect other).
-            Set time limit as args.time - 2, allowing solution select and output tasks. 
-        """
-        params_tuple = (graph, args.alg, args.time - 2, args.seed, param_json, start_time)
-        process_pool = multiprocessing.Pool(processes=defaultProcessNum)
-        # shallow copy for parameter passing
-        retrieved_sols = process_pool.starmap(solutionExecutor, [params_tuple for i in range(defaultProcessNum)])
-        # not join to allow termination, main thread keep going once having return value
-        # process_pool.join()
-        process_pool.close()
-
-        # check solution validity
-        if args.checkSol:
-            print("Checking solution validity ...")
-            for solution in range(len(retrieved_sols)):
-                if checkSol(graph_path, retrieved_sols[solution][0]):
-                    print("Solution " + str(solution) + " check: valid")
-                    continue
-                print("Solution " + str(solution) + " check: fail")
-
-        # select best
-        current_best = retrieved_sols[0]
-        for solution in range(1, len(retrieved_sols)):
-            if len(retrieved_sols[solution][0]) < len(current_best[0]):
-                current_best = retrieved_sols[solution]
-
-        write_dir = "."
-        if DEBUG:
-            write_dir = "./result"
-            if not (os.path.exists(write_dir) and os.path.isdir(write_dir)):
-                os.mkdir(write_dir)
-        write_dir += "/"
-
         # ./result/ if DEBUG else .
-        write_path = write_dir + graph_path.split("/")[-1].split(".")[0] + "_" + args.alg + "_" + str(args.time) + \
-                     (("_" + str(args.seed)) if not (args.seed == -1) else "")
-        # write vertex set
-        writeSol(writePath=write_path + ".sol", vertexSet=current_best[0])
-        # write trace
-        writeTrace(writePath=write_path + ".trace", traceList=current_best[1])
+        for run_count in range(1, args.rc + 1):
+            write_dir = "."
+            if DEBUG:
+                write_dir = os.path.join(debugResultDirectory, str(run_count))
+                if not (os.path.exists(write_dir) and os.path.isdir(write_dir)):
+                    os.mkdir(write_dir)
+            # running algorithm
+            print("Start running [" + str(run_count) + "] ...")
+            run(args, graph_path, write_dir)
+
         print("Finish running [ " + graph_path + " ] ...\n")
+
+
+def run(args, graph_path, write_dir):
+    # start running
+    start_time = time.time()
+
+    # read data
+    graph = readData(graph_path)
+    param_json = json.loads(args.params)
+
+    """
+        Using multiprocessing for possible multiple process concurrent running.
+        Multiprocessing will serialize and deserialize the argument (deep copy). 
+        Thus, each process will have it independent memory space (not affect other).
+        Set time limit as args.time - 2, allowing solution select and output tasks. 
+    """
+    params_tuple = (graph, args.alg, args.time - 2, args.seed, param_json, start_time)
+    process_pool = multiprocessing.Pool(processes=defaultProcessNum)
+    # shallow copy for parameter passing
+    retrieved_sols = process_pool.starmap(solutionExecutor, [params_tuple for i in range(defaultProcessNum)])
+    # not join to allow termination, main thread keep going once having return value
+    # process_pool.join()
+    process_pool.close()
+
+    # check solution validity
+    if args.checkSol:
+        print("Checking solution validity ...")
+        for solution in range(len(retrieved_sols)):
+            if checkSol(graph_path, retrieved_sols[solution][0]):
+                print("Solution " + str(solution) + " check: valid")
+                continue
+            print("Solution " + str(solution) + " check: fail")
+
+    # select best
+    current_best = retrieved_sols[0]
+    for solution in range(1, len(retrieved_sols)):
+        if len(retrieved_sols[solution][0]) < len(current_best[0]):
+            current_best = retrieved_sols[solution]
+
+    write_result_path = os.path.join(write_dir, graph_path.split("/")[-1].split(".")[0] + "_" + args.alg + "_"
+                                     + str(args.time) + (("_" + str(args.seed)) if not (args.seed == -1) else ""))
+    # write vertex set
+    writeSol(writePath=write_result_path + ".sol", vertexSet=current_best[0])
+    # write trace
+    writeTrace(writePath=write_result_path + ".trace", traceList=current_best[1])
 
 
 if __name__ == "__main__":
