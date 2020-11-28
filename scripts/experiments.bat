232b068a95656581b48570d627a14c00f10b450f
unknown <zhezhou@gatech.edu>
2020-11-28 12:28:04 -0500

    update experiment.bat

--- scripts/experiments.bat
+++ scripts/experiments.bat
@@ -1,3 +1,3 @@
 for /l %%x in (117, 2, 135) do (
-    python main.py -inst ./data/DATA/dummy1.graph -alg LS1 -time 600 -seed %%x -ba -rc 5
+    python main.py -inst ./data/DATA/dummy1.graph -alg LS2 -time 600 -seed %%x -ba -rc 5 -params '{\"T\":0.95,\"steps\":100000,\"alpha\":0.999}'
 )
