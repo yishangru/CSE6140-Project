a6b675f09eb3a608ccc774bced4653ce98631190
yishangru <yishangru@126.com>
2020-12-03 20:35:38 -0500

    delivery: graph init

--- deliverables/deliverable.py
+++ deliverables/deliverable.py
@@ -1,3 +1,41 @@
 """
 algorithm result analysis and graph generation
 """
+import os, shutil
+
+target_result_directory = "result"
+original_result_directory = "../result/1"
+
+# extract result file from original dir
+def extractResult(graph_name, alg):
+    if not (os.path.exists(target_result_directory) and os.path.isdir(target_result_directory)):
+        os.mkdir(target_result_directory)
+    if not (os.path.exists(original_result_directory) and os.path.isdir(original_result_directory)):
+        print("Original Result Directory Not Exist! Dir: " + original_result_directory)
+        return
+    # extract result trace and sol
+    file_list = os.listdir(original_result_directory)
+    for result in file_list:
+        file_info = result.split("_")
+        if file_info[len(file_info) - 3] == alg:
+
+
+# plot QRTD
+def qrtd(graph_name, alg, quality_list):
+    pass
+
+
+# plot SQD
+def sqd(graph_name, alg, timestamp_list):
+    pass
+
+
+# plot boxplot for runtime
+def boxplot_runtime(graph_name, alg):
+    pass
+
+
+# plot boxplot for sol size
+def boxplot_solution(graph_name, alg):
+    pass
+
--- scripts/experiments.bat
+++ scripts/experiments.bat
@@ -1,4 +1,5 @@
-for /l %%x in (117, 2, 135) do (
-    python main.py -inst ./data/DATA/dummy1.graph -alg LS2 -time 600 -seed %%x -ba -rc 5 -params '{\"T\":0.95,\"steps\":100000,\"alpha\":0.999}'
-    ::python main.py -inst ./data/DATA/dummy1.graph -alg LS1 -time 600 -seed %%x -ba -rc 5
+for /l %%x in (117, 2, 215) do (
+    ::python main.py -inst ./data/DATA/dummy1.graph -alg LS2 -time 600 -seed %%x -ba -rc 5 -params '{\"T\":0.95,\"steps\":100000,\"alpha\":0.999}'
+    python main.py -inst ./data/DATA/star.graph -alg LS1 -time 600 -seed %%x
+    python main.py -inst ./data/Data/power.graph -alg LS1 -time 600 -seed %%x
 )
